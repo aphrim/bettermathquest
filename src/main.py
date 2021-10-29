73bf1ac19d7f18f1eae9000c51e74b10bcb7e2dc
aphrim <gregperrett5@gmail.com>
2021-10-28 21:32:06 -0400

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,38 @@
+import pygame
+from pygame.locals import *
+
+
+class Game:
+    def __init__(self):
+        self.running = True
+        self.screen = init_game()
+
+    def game_loop():
+        print("Game Loop Start")
+
+    def render_loop():
+        print("Render Loop Start")
+
+    def poll_events():
+        for event in pygame.event.get():  
+            if event.type == pygame.QUIT:  
+                return False
+
+        pygame.display.flip()
+        return True
+
+    def init_game():
+        print("Game Init Start")
+        pygame.init()
+        screen = pygame.display.set_mode((400,500)) 
+        screen
+
+
+game = Game()
+
+while game.running:
+    if ( not game.poll_events() ):
+        pygame.quit()
+        break
+    game.game_loop()
+    game.render_loop()
