dd33ad9160d2a3f234c2e3d6ea34a6ea9a892c7b
aphrim <gregperrett5@gmail.com>
2021-10-30 09:27:22 -0400

    Slight Restructuring.

--- src/main.py
+++ src/main.py
@@ -1,20 +1,17 @@
 import pygame
 from pygame.locals import *
+from renderloop import render_loop
+from gameloop import game_loop
 
 class Game:
     def __init__(self):
         self.running = True
         self.screen = None
         self.pressed_keys = []
+        self.active_room = None
 
         self.init_game()
 
-    def game_loop(self):
-        self.pressed_keys = pygame.key.get_pressed()
-        print("Game Loop Start")
-
-    def render_loop(self):
-        print("Render Loop Start")
 
     def poll_events(self):
         for event in pygame.event.get():  
@@ -25,7 +22,6 @@ class Game:
         return True
 
     def init_game(self):
-        print("Game Init Start")
         pygame.init()
         screen = pygame.display.set_mode((400,500)) 
         self.screen = screen
@@ -36,5 +32,5 @@ while game.running:
     if not game.poll_events():
         pygame.quit()
         break
-    game.game_loop()
-    game.render_loop()
+    game_loop(game)
+    render_loop(game)
